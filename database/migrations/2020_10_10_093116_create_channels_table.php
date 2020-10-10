69bb2570361dfe0031afd632bbecdaf742f52155
Rafal Strab <rafal.strab@free2.com>
2020-10-10 10:47:14 +0100

    Added Channels and Programmes migrations

---
+++ database/migrations/2020_10_10_093116_create_channels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChannelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->index();
+            $table->string('name', 191);
+            $table->string('icon', 191)->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('NULL ON UPDATE CURRENT_TIMESTAMP'))->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channels');
+    }
+}
---
+++ database/migrations/2020_10_10_093128_create_programmes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgrammesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programmes', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->index();
+            $table->unsignedBigInteger('channel_id');
+            $table->string('name', 191);
+            $table->text('description')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->string('timezone')->index();
+            $table->unsignedInteger('duration');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('NULL ON UPDATE CURRENT_TIMESTAMP'))->nullable();
+
+            $table->foreign('channel_id')->references('id')->on('channels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programmes');
+    }
+}
